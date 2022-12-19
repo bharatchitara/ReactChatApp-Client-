ae3fad5f6b8371fc85372a48c435dd8babac9825
bharat chitara <bharatchitara99@gmail.com>
2022-12-19 22:12:18 +0530

    show chat button added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom"
-import Home from "./components/Home"
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Home from "./components/Home";
 import ChatPage from "./components/ChatPage";
-import socketIO from "socket.io-client"
+import socketIO from "socket.io-client";
 import Videoplayer from "./components/video";
 import '../node_modules/video-react/dist/video-react.css';
+import { useState } from "react";
 
 
 
 const socket = socketIO.connect("http://localhost:4000")
+
+let newstyle = "videoplayerstyle"
+
+
+
 function App() {
+
+  const [initialstyle , afterstyle] = useState('videoplayerstyle');
+  const [chatshow,chathide] = useState('chat');
+
+  function handleclick(){
+    afterstyle('afterchatclick');
+    chathide('chat_hide');
+  }
+
+
+
   return (
 
     <BrowserRouter>
-        <div>
-          <Videoplayer/>
+        <div className="app1">
+          <Videoplayer dataCheck = {initialstyle}/>
+          <button id='showchatbutton' onClick={handleclick}> show chat</button>
+
           <Routes>
-            {/* <Route path="/" element={<Home socket={socket}/>}></Route> */}
-            <Route path="/" element={<ChatPage socket={socket}/>}></Route>
+            <Route path="/" element={<ChatPage socket={socket} hidechatbox = {chatshow} />}></Route>
           </Routes>
+
+          {/* <ChatPage socket={socket}/> */}
+
+
     </div>
     </BrowserRouter>
     
--- src/components/video.js
+++ src/components/video.js
@@ -1,21 +1,42 @@
-import React , { Component } from 'react';
+import React , { Component , useState } from 'react';
+
 import { Player } from 'video-react';
 import '../assets/styles/video.css';
+import ChatPage from './ChatPage';
 
-export class Videoplayer extends Component{
-    render(){
+
+// export class Videoplayer extends Component{
+
+
+
+
+function Videoplayer(props){
+
+    console.log(props.dataCheck)
+
+    const [initialstyle , afterstyle] = useState('videoplayerstyle');
+
+
+    function changestyle(){
+        afterstyle("afterchatclick");
+
+    }
+
+    // render(){
         return(
-            <div className='videoplayerstyle'>
+            <div className={props.dataCheck}>
             <Player
             playsInline
             poster="/assets/poster.png"
             src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
           />
-            <button id='showchatbutton'>show chat</button>
+            {/* <button id='showchatbutton' onClick={changestyle} > show chat</button> */}
+
+            
           </div>
         )
     }
-}
+// }
 
 
 export default Videoplayer;
