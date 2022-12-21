25cb051eca8ddac2c8bcc0adafb8086ed605309e
bharat chitara <bharatchitara99@gmail.com>
2022-12-21 17:21:20 +0530

    code cleanup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom";
-import Home from "./components/Home";
 import ChatPage from "./components/ChatPage";
 import socketIO from "socket.io-client";
 import Videoplayer from "./components/video";
@@ -9,10 +8,6 @@ import { useState } from "react";
 
 
 const socket = socketIO.connect("http://localhost:4000")
-
-let newstyle = "videoplayerstyle"
-
-
 
 function App() {
 
