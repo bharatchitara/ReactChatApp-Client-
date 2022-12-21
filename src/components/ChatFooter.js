ca88f5dea70818e5fab9ff33f1273501042a447d
bharat chitara <bharatchitara99@gmail.com>
2022-12-21 17:18:14 +0530

    emoji added

--- src/components/ChatFooter.js
+++ src/components/ChatFooter.js
@@ -1,9 +1,10 @@
 import React, {useState} from 'react'
+import InputEmoji from 'react-input-emoji';
 
 const ChatFooter = ({socket}) => {
     const [message, setMessage] = useState("")
     const handleTyping = () => socket.emit("typing",`${localStorage.getItem("userName")} is typing`)
-
+    
     const handleSendMessage = (e) => {
         e.preventDefault()
         if(message.trim() && localStorage.getItem("userName")) {
@@ -21,14 +22,24 @@ const ChatFooter = ({socket}) => {
   return (
     <div className='chat__footer'>
         <form className='form' onSubmit={handleSendMessage}>
-          <input 
+          {/* <input 
             type="text" 
             placeholder='Write message' 
             className='message' 
             value={message} 
             onChange={e => setMessage(e.target.value)}
             onKeyDown={handleTyping}
-            />
+            /> */}
+
+            <InputEmoji
+                      value={message}
+                      className='message' 
+                      onChange={setMessage}
+                      placeholder="Type a message"
+                    />
+
+
+
             <button className="sendBtn">SEND</button>
         </form>
      </div>
